ac9f0cb8a4bd227fa699655c204c6fc4aa317729
BiLos <bstamariadesignblue@gmail.com>
2020-01-27 14:13:17 +0800

    done with the change convention function

--- pages/retail/promotions/create.vue
+++ pages/retail/promotions/create.vue
@@ -67,8 +67,8 @@
                                     </div>
                                     <div class="form_flex_input">
                                         <input type="text" name="start_convention" class="default_text number no_click" autocomplete="off" v-model="form.start.convention" v-validate="'required'">
-                                        <div class="up" @click="changeConvention('start', 'AM')"></div>
-                                        <div class="down" @click="changeConvention('start', 'PM')"></div>
+                                        <div class="up" @click="changeConvention('start')"></div>
+                                        <div class="down" @click="changeConvention('start')"></div>
                                         <transition name="slide"><span class="validation_errors" v-if="errors.has('start_convention')">{{ errors.first('start_convention') }}</span></transition>
                                     </div>
                                 </div>
@@ -92,8 +92,8 @@
                                     </div>
                                     <div class="form_flex_input">
                                         <input type="text" name="end_convention" class="default_text number no_click" autocomplete="off" v-model="form.end.convention" v-validate="'required'">
-                                        <div class="up" @click="changeConvention('end', 'AM')"></div>
-                                        <div class="down" @click="changeConvention('ebd', 'PM')"></div>
+                                        <div class="up" @click="changeConvention('end')"></div>
+                                        <div class="down" @click="changeConvention('end')"></div>
                                         <transition name="slide"><span class="validation_errors" v-if="errors.has('end_convention')">{{ errors.first('end_convention') }}</span></transition>
                                     </div>
                                 </div>
@@ -284,14 +284,14 @@
                     }
                 })
             },
-            changeConvention (type, status) {
+            changeConvention (type) {
                 const me = this
                 switch (type) {
                     case 'start':
-                        me.form.start.convention = status
+                        me.form.start.convention = (me.form.start.convention == 'AM') ? 'PM' : 'AM'
                         break
                     case 'end':
-                        me.form.end.convention = status
+                        me.form.start.end = (me.form.start.end == 'AM') ? 'PM' : 'AM'
                         break
                 }
             },
