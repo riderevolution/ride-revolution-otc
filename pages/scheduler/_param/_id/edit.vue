eb1cb0b17b6dbec5dc8f0c10fe18fd2d3378f6f2
BiLos <bstamariadesignblue@gmail.com>
2020-01-30 17:11:15 +0800

    scheduler

--- pages/scheduler/_param/_id/edit.vue
+++ pages/scheduler/_param/_id/edit.vue
@@ -359,26 +359,27 @@
                             formData.append('customer_type_restrictions', JSON.stringify(me.customerTypes))
                             formData.append('studios', JSON.stringify(me.studios))
                             formData.append('class_length', me.form.classLength)
-                            me.loader(true)
+                            // me.loader(true)
                             me.$axios.post(`api/schedules/${me.$route.params.id}`, formData).then(res => {
-                                setTimeout( () => {
-                                    if (res.data) {
-                                        me.notify('Content has been Updated')
-                                    } else {
-                                        me.$store.state.errorList.push('Sorry, Something went wrong')
-                                        me.$store.state.errorStatus = true
-                                    }
-                                }, 500)
+                                console.log(res.data);
+                                // setTimeout( () => {
+                                //     if (res.data) {
+                                //         me.notify('Content has been Updated')
+                                //     } else {
+                                //         me.$store.state.errorList.push('Sorry, Something went wrong')
+                                //         me.$store.state.errorStatus = true
+                                //     }
+                                // }, 500)
                             }).catch(err => {
                                 me.$store.state.errorList = err.response.data.errors
                                 me.$store.state.errorStatus = true
-                            }).then(() => {
-                                setTimeout( () => {
-                                    if (!me.$store.state.errorStatus) {
-                                        me.$router.push(`/${me.lastRoute}`)
-                                    }
-                                    me.loader(false)
-                                }, 500)
+                            // }).then(() => {
+                            //     setTimeout( () => {
+                            //         if (!me.$store.state.errorStatus) {
+                            //             me.$router.push(`/${me.lastRoute}`)
+                            //         }
+                            //         me.loader(false)
+                            //     }, 500)
                             })
                         } else {
                             me.$store.state.promptStatus = true
