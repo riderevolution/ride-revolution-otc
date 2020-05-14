6ffcb0b8d697c961a1343aea0994a5ace4f05312
BiLos <bstamariadesignblue@gmail.com>
2020-05-14 19:14:56 +0800

    added error and image in class package

--- pages/admin/classes-and-packages/class-packages-and-store-credits/class-packages/create.vue
+++ pages/admin/classes-and-packages/class-packages-and-store-credits/class-packages/create.vue
@@ -300,16 +300,21 @@
             }
         },
         filters: {
-            properFormat: function (value) {
+            properFormat (value) {
                 let newValue = value.split('The ')[1].split(' field')[0].split('[]')
                 if (newValue.length > 1) {
-                    newValue = newValue[0].charAt(0).toUpperCase() + newValue[0].slice(1)
-                }else {
+                    let nextValue = newValue[0].split('_')
+                    if (nextValue.length > 1) {
+                        newValue = nextValue[0].charAt(0).toUpperCase() + nextValue[0].slice(1) + ' ' + nextValue[1].charAt(0).toUpperCase() + nextValue[1].slice(1)
+                    } else {
+                        newValue = newValue[0].charAt(0).toUpperCase() + newValue[0].slice(1)
+                    }
+                } else {
                     newValue = value.split('The ')[1].split(' field')[0].split('_')
                     if (newValue.length > 1) {
                         let firstValue = ''
                         let lastValue = ''
-                        if (newValue[0] != 'ao' && newValue[0] != 'por') {
+                        if (newValue[0] != 'co' && newValue[0] != 'pa' && newValue[0] != 'ec' && newValue[0] != 'ba') {
                             firstValue = newValue[0].charAt(0).toUpperCase() + newValue[0].slice(1)
                         }
                         for (let i = 1; i < newValue.length; i++) {
@@ -327,7 +332,12 @@
                     message = message[1]
                     return `The ${newValue} field${message}`
                 } else {
-                    return `The ${newValue}`
+					if (message[0].split('file').length > 1) {
+                        message = message[0].split('file')[1]
+                        return `The ${newValue} field${message}`
+                    } else {
+                        return `The ${newValue}`
+                    }
                 }
             }
         },
