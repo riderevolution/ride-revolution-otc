06c1b4402c3d2c4e534ae8248bd33758c80acfb5
BiLos <bstamariadesignblue@gmail.com>
2020-06-15 18:27:59 +0800

    fixed min-value partially

--- components/PartialTimeHandler.vue
+++ components/PartialTimeHandler.vue
@@ -3,7 +3,7 @@
         <div v-if="$parent.showCloser" class="form_close alternate" @click="removeImage()"></div>
         <div class="form_group">
             <label for="start_time">Start Time <span>*</span></label>
-            <input type="time" name="start_time[]" :min="$moment().format('HH:mm')" v-validate="'required'" class="default_text" @change="getFrom($event)">
+            <input type="time" name="start_time[]" v-validate="'required'" class="default_text" @change="getFrom($event)">
             <transition name="slideY"><span class="validation_errors" v-if="errors.has('start_time[]')">{{ errors.first('start_time[]') | properFormat }}</span></transition>
         </div>
         <div class="form_group">
