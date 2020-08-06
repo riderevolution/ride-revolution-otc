b0e39439af2e7551e0e361726ab28b3967eb8c01
teryong <dthcrpz@gmail.com>
2020-08-06 18:31:49 +0800

    fixed refundable

--- components/Variant.vue
+++ components/Variant.vue
@@ -42,7 +42,7 @@
             </td>
             <td>
                 <div class="form_check">
-                    <input type="checkbox" :id="`refundable_${unique}`" name="refundable[]" class="action_check" :checked="(value.refundable == 1) ? true : false">
+                    <input type="checkbox" :id="`refundable_${unique}`" :name="`refundable[${unique}]`" class="action_check" :checked="(value.refundable == 1) ? true : false">
                     <label :for="`refundable_${unique}`">Refundable</label>
                 </div>
             </td>
