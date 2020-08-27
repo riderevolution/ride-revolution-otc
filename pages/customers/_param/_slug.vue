07d32eb96ac688c15c3f5100a3f5616ad58fe48d
BiLos <bstamariadesignblue@gmail.com>
2020-08-27 15:08:54 +0800

    fixed some conditions in the customer dashbaord

--- pages/customers/_param/_slug.vue
+++ pages/customers/_param/_slug.vue
@@ -20,7 +20,7 @@
                                 </span>
                             </h1>
                             <div class="user_contact">
-                                <a :href="`tel:${customer.customer_details.co_contact_number}`" class="number">{{ customer.customer_details.co_contact_number }}</a>
+                                <a :href="`tel:${customer.customer_details.co_contact_number}`" class="number" v-if="customer.customer_details.co_contact_number">{{ customer.customer_details.co_contact_number }}</a>
                                 <a :href="`mailto:${customer.email}`" class="email">{{ customer.email }}</a>
                             </div>
                             <div class="user_summary">
