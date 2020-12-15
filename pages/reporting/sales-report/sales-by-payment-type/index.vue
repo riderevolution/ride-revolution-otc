b15c5b21e7516fba82248f4c8b1f92951b62134e
BiLos <bstamariadesignblue@gmail.com>
2020-12-15 11:04:15 +0800

    refactored sales by payment type landing

--- pages/reporting/sales-report/sales-by-payment-type/index.vue
+++ pages/reporting/sales-report/sales-by-payment-type/index.vue
@@ -13,13 +13,19 @@
                         </div>
                         <div class="actions">
                             <a :href="`/print/reporting/sales/payment-type?payment_status=${payment_status}&studio_id=${form.studio_id}&start_date=${form.start_date}&end_date=${form.end_date}`" target="_blank" class="action_btn alternate">Print</a>
+
+                            <div class="action_btn alternate" @click="getDailyTransactions()" v-if="res.length > 0">
+                                Export
+                            </div>
+
                             <download-csv
                                 v-if="res.length > 0"
-                                class="action_btn alternate margin"
+                                class="hidden me"
                                 :data="paymentTypeAttributes"
                                 :name="`sales-by-payment-type-${$moment(form.start_date).format('MM-DD-YY')}-${$moment(form.end_date).format('MM-DD-YY')}.csv`">
                                 Export
                             </download-csv>
+
                         </div>
                     </div>
                     <div class="filter_wrapper">
@@ -91,7 +97,7 @@
                             </tr>
                         </tbody>
                     </table>
-                    <div class="cms_table_toggler">
+                    <!-- <div class="cms_table_toggler">
                         <a :href="`/print/reporting/sales/payment-type/register-sales-summary?payment_status=${payment_status}&studio_id=${form.studio_id}&start_date=${form.start_date}&end_date=${form.end_date}`" target="_blank" class="action_btn alternate">Print</a>
                         <download-csv
                             v-if="studio_res.length > 0"
@@ -100,7 +106,7 @@
                             :name="`register-sales-summary-${$moment().format('MM-DD-YY-hh-mm')}.csv`">
                             Export
                         </download-csv>
-                    </div>
+                    </div> -->
                     <table class="cms_table alt">
                         <thead>
                             <tr>
@@ -180,34 +186,180 @@
                 const me = this
                 return [
                     ...me.values.map(value => ({
-                        'Studio': this.getStudio(),
-                        'Payment': value.name,
-                        'Payment Status': me.payment_status,
-                        'Transaction Count': value.transaction_count,
-                        'Gross Receipts': `Php ${(value.gross_receipts) ? value.gross_receipts : 0}`,
-                        'Gross Refunds': `Php ${(value.gross_refunds) ? value.gross_refunds : 0}`,
-                        'Sales Tax': `Php ${(value.sales_tax) ? value.sales_tax : 0}`,
-                        'Refund Tax': `Php ${(value.sales_tax) ? value.sales_tax : 0}`,
-                        'Net of Receipts': `Php ${(value.net_receipts) ? value.net_receipts : 0}`,
-                        'Net of Refunds': `Php ${(value.net_refunds) ? value.net_refunds : 0}`
+                        'Studio': me.getPaymentStudio(value.parent),
+                        'Customer': `${value.parent.user.first_name} ${value.parent.user.last_name}`,
+                        'Email Address': value.parent.user.email,
+                        'Contact Number': (value.parent.user.customer_details.co_contact_number != null) ? value.parent.user.customer_details.co_contact_number : (value.parent.user.customer_details.ec_contact_number) ? value.parent.user.customer_details.ec_contact_number : 'N/A' ,
+                        'Payment ID': value.parent.id,
+                        'Reference Number': me.getPaymentCode(value.parent),
+                        'Transaction Date': me.$moment(value.parent.updated_at).format('MMMM DD, YYYY hh:mm A'),
+                        'Promo Code': (value.parent.promo_code_used != null) ? value.parent.promo_code_used : 'No Promo Code Used',
+                        'Payment Status': value.parent.status,
+                        'Payment Method': me.replacer(value.parent.payment_method.method),
+                        'Payment Item Id': value.id,
+                        'SKU ID': me.getPaymentItem(value, 'sku'),
+                        'Item': me.getPaymentItem(value, 'name'),
+                        'Item Category': (value.product_variant) ? value.product_variant.product.category.name : 'N/A',
+                        'Quantity': value.quantity,
+                        'Discount': `${(value.parent.promo_code_used != null) ? value.parent.discount.discount : 0}`,
+                        'Price': `${(value.parent.promo_code_used != null) ? value.total : value.price_per_item}`,
+                        'Employee': me.getPaymentDetails(value.parent, 'employee'),
+                        'Comp Reason': (value.parent.comp_reason) ? value.parent.comp_reason : 'N/A',
+                        'Note': (value.parent.note) ? value.parent.note : 'N/A',
+                        'Remarks': (value.parent.remarks) ? value.parent.remarks : 'N/A'
                     }))
                 ]
             },
-            registerSalesSummaryAttributes () {
-                const me = this
-                return [
-                    ...me.summary_values.map(value => ({
-                        'Studio': value.name,
-                        'Subtotal': `Php ${(value.subtotal) ? value.subtotal : 0}`,
-                        'Tax': `Php ${(value.total_tax) ? value.total_tax : 0}`,
-                        'Refund Subtotal': `Php ${(value.subtotal_refund) ? value.subtotal_refund : 0}`,
-                        'Refund Tax': `Php ${(value.total_tax) ? value.total_tax : 0}`,
-                        'Total': `Php ${(value.total_income) ? value.total_income : 0}`
-                    }))
-                ]
-            }
+            // registerSalesSummaryAttributes () {
+            //     const me = this
+            //     return [
+            //         ...me.summary_values.map(value => ({
+            //             'Studio': value.name,
+            //             'Subtotal': `Php ${(value.subtotal) ? value.subtotal : 0}`,
+            //             'Tax': `Php ${(value.total_tax) ? value.total_tax : 0}`,
+            //             'Refund Subtotal': `Php ${(value.subtotal_refund) ? value.subtotal_refund : 0}`,
+            //             'Refund Tax': `Php ${(value.total_tax) ? value.total_tax : 0}`,
+            //             'Total': `Php ${(value.total_income) ? value.total_income : 0}`
+            //         }))
+            //     ]
+            // }
         },
         methods: {
+            getDailyTransactions () {
+                const me = this
+                me.loader (true)
+                me.values = []
+                let formData = new FormData(document.getElementById('filter'))
+                me.$axios.post(`api/reporting/sales/sales-and-transactions/sales-summary/daily?all=1`, formData).then(res => {
+                    if (res.data) {
+                        res.data.payments.forEach((parent, key) => {
+                            parent.payment_items.forEach((child, key) => {
+                                child.parent = parent
+                                me.values.push(child)
+                            })
+                        })
+                    }
+                }).catch(err => {
+                    me.$store.state.errorList = err.response.data.errors
+                    me.$store.state.errorStatus = true
+                }).then(() => {
+                    me.loader(false)
+                    document.querySelector('.me').click()
+                })
+            },
+            getPaymentItem (payment_item, type) {
+                const me = this
+                let result = ''
+
+                if (type == 'sku') {
+                    switch (payment_item.type) {
+                        case 'class-package':
+                        case 'promo-package':
+                            result = payment_item.class_package.sku_id
+                            break
+                        case 'product-variant':
+                            result = payment_item.product_variant.sku_id
+                            break
+                        case 'custom-gift-card':
+                            result = payment_item.gift_card.card_code
+                            break
+                        case 'physical-gift-card':
+                            result = payment_item.gift_card.sku_id
+                            break
+                        case 'store-credit':
+                            result = payment_item.store_credit.sku_id
+                            break
+                    }
+                } else {
+                    switch (payment_item.type) {
+                        case 'class-package':
+                        case 'promo-package':
+                            result = payment_item.class_package.name
+                            break
+                        case 'product-variant':
+                            result = `${payment_item.product_variant.product.name} ${payment_item.product_variant.variant}`
+                            break
+                        case 'custom-gift-card':
+                            result = `Digital Gift Card - ${payment_item.gift_card.card_code}`
+                            break
+                        case 'physical-gift-card':
+                            result = `Physical Gift Card - ${payment_item.gift_card.card_code}`
+                            break
+                        case 'store-credit':
+                            result = payment_item.store_credit.name
+                            break
+                    }
+                }
+
+                return result
+            },
+            getPaymentDetails (payment, type) {
+                const me = this
+                let result = 0
+
+                payment.payment_items.forEach((payment_item, key) => {
+                    switch (type) {
+                        case 'qty':
+                            result += payment_item.quantity
+                            break
+                    }
+                })
+
+                switch (type) {
+                    case 'qty':
+                        result = me.totalItems(result)
+                        break
+                    case 'price':
+                        let temp_price = 0
+                        payment.payment_items.forEach((payment_item, key) => {
+                            if (payment.promo_code_used !== null) {
+                                temp_price += parseInt(payment_item.total)
+                            } else {
+                                temp_price += parseInt(payment_item.price_per_item)
+                            }
+                        })
+                        result = `Php ${me.totalCount(temp_price)}`
+                        break
+                    case 'employee':
+                        if (payment.employee != null) {
+                            result = `${payment.employee.first_name} ${payment.employee.last_name}`
+                        } else {
+                            result = 'No User'
+                        }
+                        break
+                }
+
+                return result
+            },
+            getPaymentStudio (payment) {
+                const me = this
+                let result = ''
+
+                if (payment.studio != null) {
+                    result = payment.studio.name
+                } else {
+                    result = 'Website/Online'
+                }
+
+                return result
+            },
+            getPaymentCode (payment) {
+                const me = this
+                let result = ''
+
+                switch (payment.payment_method.method) {
+                    case 'paypal':
+                        result = payment.payment_method.paypal_transaction_id
+                        break
+                    case 'paymaya':
+                        result = payment.payment_method.paymaya_transaction_id
+                        break
+                    default:
+                        result = payment.payment_code
+                }
+
+                return result
+            },
             getStudio () {
                 const me = this
                 let result = ''
@@ -251,16 +403,11 @@
                             me.payment_total = res.data.payment_grand_total
                             me.studio_res = res.data.studio_sales_summary
                             me.studio_total = res.data.studio_grand_total
-
-                            res.data.result.forEach((item, i) => {
-                                me.values.unshift(item)
-                            })
-                            me.values.push(res.data.payment_grand_total)
-
-                            res.data.studio_sales_summary.forEach((item, i) => {
-                                me.summary_values.unshift(item)
-                            })
-                            me.summary_values.push(res.data.studio_grand_total)
+                            //
+                            // res.data.studio_sales_summary.forEach((item, i) => {
+                            //     me.summary_values.unshift(item)
+                            // })
+                            // me.summary_values.push(res.data.studio_grand_total)
 
                         }, 500)
                     }
@@ -291,18 +438,13 @@
                             me.res = res.data.result
                             me.payment_total = res.data.payment_grand_total
 
-                            res.data.result.forEach((item, i) => {
-                                me.values.push(item)
-                            })
-                            me.values.push(res.data.payment_grand_total)
-
                             me.studio_res = res.data.studio_sales_summary
                             me.studio_total = res.data.studio_grand_total
-
-                            res.data.studio_sales_summary.forEach((item, i) => {
-                                me.summary_values.push(item)
-                            })
-                            me.summary_values.push(res.data.studio_grand_total)
+                            //
+                            // res.data.studio_sales_summary.forEach((item, i) => {
+                            //     me.summary_values.push(item)
+                            // })
+                            // me.summary_values.push(res.data.studio_grand_total)
 
                             me.$axios.get('api/studios', {
                                 headers: {
