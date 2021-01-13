70d1dd85774ff46a727b3d19eb01020534e9a736
BiLos <bstamariadesignblue@gmail.com>
2021-01-13 15:20:02 +0800

    done in the sales and transactions

--- pages/reporting/sales-report/sales-and-transactions/index.vue
+++ pages/reporting/sales-report/sales-and-transactions/index.vue
@@ -11,22 +11,15 @@
                             </div>
                         </div>
                         <div class="actions">
-                            <a :href="`/print/${apiRoute}?status=${tabStatus}&studio_id=${form.studio_id}&start_date=${form.start_date}&end_date=${form.end_date}&filtered=${filtered}`" target="_blank" class="action_btn alternate" v-if="(tabStatus != 'summary' && tabStatus != 'daily') && user.staff_details.role_id == 1">Print</a>
+                            <!-- <a :href="`/print/${apiRoute}?status=${tabStatus}&studio_id=${form.studio_id}&start_date=${form.start_date}&end_date=${form.end_date}&filtered=${filtered}`" target="_blank" class="action_btn alternate" v-if="(tabStatus != 'summary' && tabStatus != 'daily') && user.staff_details.role_id == 1">Print</a> -->
 
-                            <a :href="`/print/reporting/sales/sales-and-transactions/daily-transactions?status=daily&studio_id=${form.studio_id}&start_date=${form.start_date}&end_date=${form.end_date}`" target="_blank" class="action_btn alternate" v-if="tabStatus == 'daily'">Print</a>
+                            <a :href="`/print/reporting/sales/sales-and-transactions/daily-transactions?status=daily&studio_id=${form.studio_id}&start_date=${form.start_date}&end_date=${form.end_date}`" target="_blank" class="action_btn alternate" v-if="tabStatus != 'sales-summary'">Print</a>
 
-                            <download-csv
-                                v-if="(tabStatus != 'summary' && tabStatus != 'daily') && user.staff_details.role_id == 1"
-                                class="action_btn alternate"
-                                :data="salesSummaryAttributes"
-                                :name="`${tabStatus}-summary-${$moment(form.start_date).format('MM-DD-YY')}-${$moment(form.end_date).format('MM-DD-YY')}.csv`">
-                                Export
-                            </download-csv>
-                            <div class="action_btn alternate" @click="getDailyTransactions()" v-if="tabStatus == 'daily' && transactions.payments.data.length > 0">
+                            <div class="action_btn alternate" @click="getDailyTransactions()" v-if="tabStatus != 'sales-summary' && transactions.payments.data.length > 0">
                                 Export
                             </div>
                             <download-csv
-                                v-if="tabStatus == 'daily' && transactions.payments.data.length > 0"
+                                v-if="tabStatus != 'sales-summary' && transactions.payments.data.length > 0"
                                 class="hidden me"
                                 :data="dailyTransactionAttributes"
                                 :name="`daily-transactions-${$moment(form.start_date).format('MM-DD-YY')}-${$moment(form.end_date).format('MM-DD-YY')}.csv`">
@@ -126,7 +119,7 @@
                             </tbody>
                         </table>
                     </div>
-                    <div v-else-if="slug == 'daily'">
+                    <div v-else>
                         <table class="cms_table_accordion">
                             <thead>
                                 <tr>
@@ -134,10 +127,10 @@
                                     <th>Qty.</th>
                                     <th>Transaction Date</th>
                                     <th>Method</th>
+                                    <th>Status</th>
                                     <th>Gross Price</th>
                                     <th>Discount Price</th>
                                     <th>Net Price</th>
-                                    <th>Status</th>
                                     <th>Studio</th>
                                     <th>Comp Reason</th>
                                     <th>Note</th>
@@ -148,7 +141,7 @@
                             <tbody :class="`tbp ${(data.open) ? 'toggled' : ''}`" v-for="(data, key) in transactions.payments.data" v-if="transactions.payments.data.length > 0">
                                 <tr class="parent">
                                     <td class="toggler" @click.self="toggleAccordion($event, key)">
-                                        <div class="thumb">
+                                        <div class="thumb" v-if="data.user">
                                             <img :src="data.user.customer_details.images[0].path_resized" v-if="data.user.customer_details.images[0].path != null" />
                                             <div class="table_image_default" v-else>
                                                 <div class="overlay">
@@ -157,19 +150,20 @@
                                             </div>
                                             <div class="table_data_link" @click="openWindow(`/customers/${data.user.id}/packages`)">{{ data.user.fullname }}</div>
                                         </div>
+                                        No Customer
                                     </td>
                                     <td>{{ getPaymentDetails(data, 'qty') }}</td>
                                     <td>{{ $moment(data.updated_at).format('MMM DD, YYYY hh:mm A') }}</td>
                                     <td class="capitalize">{{ replacer(data.payment_method.method) }}</td>
-                                    <td>Php {{ totalCount(data.gross) }}</td>
-                                    <td>Php {{ totalCount(data.discount) }}</td>
-                                    <td>Php {{ totalCount(data.net) }}</td>
                                     <td>
                                         <div class="table_actions">
                                             <div :class="`action_status ${(data.status == 'paid') ? 'green' : 'red' }`">{{ data.status }}</div>
                                             <div class="action_status red ml" v-if="data.promo_code_used !== null">Discounted</div>
                                         </div>
                                     </td>
+                                    <td>Php {{ totalCount(data.gross) }}</td>
+                                    <td>Php {{ totalCount(data.discount) }}</td>
+                                    <td>Php {{ totalCount(data.net) }}</td>
                                     <td>{{ getPaymentStudio(data) }}</td>
                                     <td>{{ (data.payment_method.comp_reason) ? data.payment_method.comp_reason : 'N/A' }}</td>
                                     <td>{{ (data.payment_method.note) ? data.payment_method.note : 'N/A' }}</td>
@@ -225,65 +219,6 @@
                             </tbody>
                         </table>
                         <pagination :apiRoute="transactions.payments.path" :current="transactions.payments.current_page" :last="transactions.payments.last_page" v-if="transactions.payments.data.length > 0" />
-                    </div>
-                    <div v-else>
-                        <table class="cms_table alt">
-                            <thead>
-                                <tr>
-                                    <th class="cms_table_title">Items</th>
-                                    <th colspan="3" class="cms_table_title">Income</th>
-                                    <th colspan="6" class="cms_table_title">Mode of Payment</th>
-                                </tr>
-                                <tr>
-                                    <th>Items</th>
-                                    <th>Sum</th>
-                                    <th>ITY</th>
-                                    <th>ITD</th>
-                                    <th>CA</th>
-                                    <th>GC</th>
-                                    <th>CC</th>
-                                    <th>DC/EPS</th>
-                                    <th>CQ</th>
-                                    <th>PP</th>
-                                    <th>PM</th>
-                                    <!-- <th>RC</th> -->
-                                    <th>SC</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="green">Total</td>
-                                    <td class="green">Php {{ totalCount(res.item_total.totalSum) }}</td>
-                                    <td class="green">Php {{ totalCount(res.item_total.totalITY) }}</td>
-                                    <td class="green">Php {{ totalCount(res.item_total.totalITD) }}</td>
-                                    <td class="green">{{ res.item_payment_mode_total.cash }}</td>
-                                    <td class="green">{{ res.item_payment_mode_total.gcash }}</td>
-                                    <td class="green">{{ res.item_payment_mode_total.creditCard }}</td>
-                                    <td class="green">{{ res.item_payment_mode_total.debitCard }}</td>
-                                    <td class="green">{{ res.item_payment_mode_total.check }}</td>
-                                    <td class="green">{{ res.item_payment_mode_total.paypal }}</td>
-                                    <td class="green">{{ res.item_payment_mode_total.paymaya }}</td>
-                                    <td class="green">{{ res.item_payment_mode_total.storeCredit }}</td>
-                                </tr>
-                                <tr v-for="(data, key) in res.items" :key="key" v-if="data.sum > 0">
-                                    <td>
-                                        <div class="table_data_link">{{ (data.card_code) ? data.card_code : (data.variant ? data.variant : data.name) }}</div>
-                                    </td>
-                                    <td>Php {{ (data.sum) ? totalCount(data.sum) : 0 }}</td>
-                                    <td>Php {{ totalCount(data.ITY) }}</td>
-                                    <td>Php {{ totalCount(data.ITD) }}</td>
-                                    <td>{{ (data.paymentModes) ? data.paymentModes.cash : 0 }}</td>
-                                    <td>{{ (data.paymentModes) ? data.paymentModes.gcash : 0 }}</td>
-                                    <td>{{ (data.paymentModes) ? data.paymentModes.creditCard : 0 }}</td>
-                                    <td>{{ (data.paymentModes) ? data.paymentModes.debitCard : 0 }}</td>
-                                    <td>{{ (data.paymentModes) ? data.paymentModes.check : 0 }}</td>
-                                    <td>{{ (data.paymentModes) ? data.paymentModes.paypal : 0 }}</td>
-                                    <td>{{ (data.paymentModes) ? data.paymentModes.paymaya : 0 }}</td>
-                                    <td>{{ (data.paymentModes) ? data.paymentModes.storeCredit : 0 }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <!-- <pagination :apiRoute="transactions.payments.path" :current="transactions.payments.current_page" :last="transactions.payments.last_page" v-if="transactions.payments.data.length > 0" /> -->
                     </div>
                 </section>
             </div>
@@ -368,27 +303,6 @@
                         'Note': (value.parent.payment_method.note) ? value.parent.payment_method.note : 'N/A',
                         'Remarks': (value.parent.remarks) ? value.parent.remarks : 'N/A',
                         'Username': me.getPaymentDetails(value.parent, 'employee')
-                    }))
-                ]
-            },
-            salesSummaryAttributes () {
-                const me = this
-                let current_length = me.sales_summary_values.length - 1
-                return [
-                    ...me.sales_summary_values.map((value, key) => ({
-                        'Studio': me.getStudio(),
-                        'Items': (key == current_length) ? 'Total' : (value.card_code) ? value.card_code : (value.variant ? value.variant : value.name),
-                        'Qty': (key == current_length) ? value.totalQty : (value.qty ? value.qty : 0),
-                        'ITY': `Php ${(key == current_length) ? me.totalCount(value.totalITY) : me.totalCount(value.ITY)}`,
-                        'ITD': `Php ${(key == current_length) ? me.totalCount(value.totalITD) : me.totalCount(value.ITD)}`,
-                        'CA': (key == current_length) ? me.res.item_payment_mode_total.cash : (value.paymentModes) ? value.paymentModes.cash : 0,
-                        'GC': (key == current_length) ? me.res.item_payment_mode_total.gcash : (value.paymentModes) ? value.paymentModes.cash : 0,
-                        'CC': (key == current_length) ? me.res.item_payment_mode_total.creditCard : (value.paymentModes) ? value.paymentModes.cash : 0,
-                        'DC/EPS': (key == current_length) ? me.res.item_payment_mode_total.debitCard : (value.paymentModes) ? value.paymentModes.cash : 0,
-                        'CQ': (key == current_length) ? me.res.item_payment_mode_total.check : (value.paymentModes) ? value.paymentModes.cash : 0,
-                        'PP': (key == current_length) ? me.res.item_payment_mode_total.paypal : (value.paymentModes) ? value.paymentModes.cash : 0,
-                        'PM': (key == current_length) ? me.res.item_payment_mode_total.paymaya : (value.paymentModes) ? value.paymentModes.cash : 0,
-                        'SC': (key == current_length) ? me.res.item_payment_mode_total.storeCredit : (value.paymentModes) ? value.paymentModes.cash : 0
                     }))
                 ]
             }
@@ -584,18 +498,7 @@
                                 me.res.income_breakdown = res.data.incomeBreakdown
                                 me.res.income_breakdown_total = res.data.incomeBreakdownTotal
                             } else {
-                                if (me.slug == 'daily') {
-                                    me.transactions = res.data
-                                } else {
-                                    me.res.items = res.data.items
-                                    me.res.item_total = res.data.total
-                                    me.res.item_payment_mode_total = res.data.paymentModesTotal
-
-                                    res.data.items.forEach((item, i) => {
-                                        me.sales_summary_values.push(item)
-                                    })
-                                    me.sales_summary_values.push(res.data.total)
-                                }
+                                me.transactions = res.data
                             }
                         }, 500)
                     }
@@ -629,18 +532,7 @@
                                 me.res.income_breakdown = res.data.incomeBreakdown
                                 me.res.income_breakdown_total = res.data.incomeBreakdownTotal
                             } else {
-                                if (me.slug == 'daily') {
-                                    me.transactions = res.data
-                                } else {
-                                    me.res.items = res.data.items
-                                    me.res.item_total = res.data.total
-                                    me.res.item_payment_mode_total = res.data.paymentModesTotal
-
-                                    res.data.items.forEach((item, i) => {
-                                        me.sales_summary_values.push(item)
-                                    })
-                                    me.sales_summary_values.push(res.data.total)
-                                }
+                                me.transactions = res.data
                             }
                             me.filtered = true
                         }, 500)
