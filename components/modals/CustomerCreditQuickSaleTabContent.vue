ed373e7477baa2af23cbe398a763c97f77210251
BiLos <bstamariadesignblue@gmail.com>
2021-04-08 15:33:18 +0800

    if admin show deactivate in quick sale

--- components/modals/CustomerCreditQuickSaleTabContent.vue
+++ components/modals/CustomerCreditQuickSaleTabContent.vue
@@ -15,7 +15,7 @@
                     <transition name="slide"><span class="validation_errors" v-if="errors.has('product_form.quantity[]')">{{ properFormat(errors.first('product_form.quantity[]')) }}</span></transition>
                 </div>
             </div>
-            <div class="table_action_cancel link" @click="$parent.toggleDeactivate(value.id)">Deactivate</div>
+            <div class="table_action_cancel link" @click="$parent.toggleDeactivate(value.id)" v-if="$parent.user.staff_details.role_id == 1">Deactivate</div>
         </div>
     </div>
 </template>
