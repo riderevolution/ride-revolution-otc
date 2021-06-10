df8ca620b51597642f09fd674cac2c096ea15d89
BiLosxD <billyrosssta.maria@gmail.com>
2021-06-10 15:12:20 +0800

    changed api

--- pages/reporting/class-report/attendance-by-month/index.vue
+++ pages/reporting/class-report/attendance-by-month/index.vue
@@ -206,27 +206,30 @@
                 const me = this
                 let result = ''
                 if (data != '') {
-                    let ins_ctr = 0, instructor = []
+                    let ins_ctr = 0, instructor = [], subInstructor = [], targetInstructor = []
                     data.schedule.instructor_schedules.forEach((ins, index) => {
                         if (ins.substitute == 0) {
                             ins_ctr += 1
+                            subInstructor = ins
                         }
                         if (ins.primary == 1) {
                             instructor = ins
                         }
                     })
 
+                    targetInstructor = (instructor != []) ? instructor : subInstructor
+
                     if (ins_ctr == 2) {
                         if (export_status != null) {
-                            result = `${instructor.user.instructor_details.nickname} + ${data.schedule.instructor_schedules[1].user.instructor_details.nickname}`
+                            result = `${targetInstructor.user.instructor_details.nickname} + ${data.schedule.instructor_schedules[1].user.instructor_details.nickname}`
                         } else {
-                            result = `<b>${instructor.user.instructor_details.nickname} + ${data.schedule.instructor_schedules[1].user.instructor_details.nickname}</b> <b class="g">(${data.schedule.class_type.name})</b>`
+                            result = `<b>${targetInstructor.user.instructor_details.nickname} + ${data.schedule.instructor_schedules[1].user.instructor_details.nickname}</b> <b class="g">(${data.schedule.class_type.name})</b>`
                         }
                     } else {
                         if (export_status != null) {
-                            result = `${instructor.user.fullname}`
+                            result = `${(targetInstructor.user) ? targetInstructor.user.fullname : 'No Instructor Set'}`
                         } else {
-                            result = `<b>${instructor.user.fullname}</b> <b class="g">(${data.schedule.class_type.name})</b>`
+                            result = `<b>${(targetInstructor.user) ? targetInstructor.user.fullname : 'No Instructor Set'}</b> <b class="g">(${data.schedule.class_type.name})</b>`
                         }
                     }
                 }
@@ -248,10 +251,9 @@
                 formData.append('start_date', startDate)
                 formData.append('end_date', endDate)
 
-
                 me.values = []
                 me.loader(true)
-                me.$axios.post(`api/reporting/classes/attendance-with-revenue?for_export=1`, formData).then(res => {
+                me.$axios.post(`api/exports/class-report/attendance-by-month`, formData).then(res => {
                     if (res.data) {
 
                         me.values = res.data.export_data
