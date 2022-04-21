3213cbb08630113578f43a1723c6f50b090360b5
BiLosxD <billyrosssta.maria@gmail.com>
2022-04-21 17:23:36 +0800

    try text the remaining class package revenue

--- pages/reporting/class-package-report/remaining-class-package-value/index.vue
+++ pages/reporting/class-package-report/remaining-class-package-value/index.vue
@@ -160,7 +160,7 @@
                         'Reference Number': me.getPaymentCode(value),
                         'Payment Method': (value.payment_item.payment_method) ? value.payment_item.payment_method.method : 'N/A',
                         'Class Package': value.class_package.name,
-                        'Class Package Price': value.payment_item.price_per_item,
+                        'Class Package Price': (value.payment.promo_code_used != null) ? value.payment_item.total : value.payment_item.price_per_item,
                         'Estimated Price Per Class': value.estimated_price_per_class,
                         'Original Count': value.original_package_count,
                         'Used Count': value.original_package_count - value.count,
