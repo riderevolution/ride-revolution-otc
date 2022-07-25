3d45a883d3853659004572a68fa993f9ab085601
BiLosxD <billyrosssta.maria@gmail.com>
2022-07-25 17:31:08 +0800

    refactorization of remaining class package

--- pages/reporting/class-package-report/remaining-class-package-value/index.vue
+++ pages/reporting/class-package-report/remaining-class-package-value/index.vue
@@ -47,8 +47,7 @@
                 <section id="content">
                     <div class="cms_table_toggler">
                         <div :class="`status ${(tab == 'studio') ? 'active' : ''}`" @click="toggleStatus('studio')">Studio</div>
-                        <div :class="`status ${(tab == 'online') ? 'active' : ''}`" @click="toggleStatus('online')">Online</div>
-                        <div :class="`status ${(tab == 'os') ? 'active' : ''}`" @click="toggleStatus('os')">Website/Online Sales</div>
+                        <div :class="`status ${(tab == 'online') ? 'active' : ''}`" @click="toggleStatus('online')">Online Class/Online Sales</div>
                     </div>
                     <table class="cms_table alt">
                         <thead>
