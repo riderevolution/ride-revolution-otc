a9b780420a6b6d06106a104afd50de7a7b884f26
BiLosxD <billyrosssta.maria@gmail.com>
2023-02-14 14:39:31 +0800

    STAGING: Added waitlist manualrefresh

--- components/SeatPlan.vue
+++ components/SeatPlan.vue
@@ -1,111 +1,131 @@
 <template>
-    <div class="plan_wrapper">
-        <seat-plan-box v-for="(seat, key) in seats" :data="seat.data" :customer="customer" :position="seat.position" :layout="seat.layout" :total="ctr" :key="key" v-if="hasFetchSeat && !onlineClass" />
-        <div class="no_results" v-if="!hasFetchSeat">
-            Please select a class first.
-        </div>
+  <div class="plan_wrapper">
+    <seat-plan-box
+      v-for="(seat, key) in seats"
+      :data="seat.data"
+      :customer="customer"
+      :position="seat.position"
+      :layout="seat.layout"
+      :total="ctr"
+      :key="key"
+      v-if="hasFetchSeat && !onlineClass"
+    />
+    <div class="no_results" v-if="!hasFetchSeat">
+      Please select a class first.
     </div>
+  </div>
 </template>
 
 <script>
-    import SeatPlanBox from './SeatPlanBox'
-    export default {
-        components: {
-            SeatPlanBox
-        },
-        props: {
-            customer: {
-                default: null
-            },
-            onlineClass: {
-                type: Boolean,
-                default: false
-            }
-        },
-        data () {
-            return {
-                assignType: 0,
-                ctr: 0,
-                temp: [],
-                hasCustomer: false,
-                hasCancel: false,
-                message: '',
-                hasFetchSeat: false,
-                seats: {
-                    left: {
-                        position: 'left',
-                        data: []
-                    },
-                    right: {
-                        position: 'right',
-                        data: []
-                    },
-                    bottom: {
-                        position: 'bottom',
-                        data: []
-                    },
-                    bottom_alt: {
-                        position: 'bottom_alt',
-                        data: []
-                    },
-                    bottom_alt_2: {
-                        position: 'bottom_alt_2',
-                        data: []
-                    },
-                }
-            }
-        },
-        methods: {
-            toggleOnlineAttendance () {
-                const me = this
-                me.$store.state.onlineAttendanceLayoutStatus = true
-            },
-            fetchSeats (schedule_id, id) {
-                const me = this
-                let layout = `layout_${id}`, temp_ctr = 0
-                me.seats = { left: { position: 'left', layout: layout, data: [] }, right: { position: 'right', layout: layout, data: [] }, bottom: { position: 'bottom', layout: layout, data: [] }, bottom_alt: { position: 'bottom_alt', layout: layout, data: [] }, bottom_alt_2: { position: 'bottom_alt_2', layout: layout, data: [] }, }
-                if (schedule_id != null) {
-                    me.loader(true)
-                    me.$axios.get(`api/seats?studio_id=${id}&scheduled_date_id=${schedule_id}`).then(res => {
-                        if (res.data) {
-                            me.temp = res.data.seats
-                            me.temp.forEach((seat , index) => {
-                                if (seat.bookings.length > 0) {
-                                    temp_ctr += 1
-                                }
-                                switch (seat.position) {
-                                    case 'left':
-                                        me.seats.left.data.push(seat)
-                                        break
-                                    case 'right':
-                                        me.seats.right.data.push(seat)
-                                        break
-                                    case 'bottom':
-                                        me.seats.bottom.data.push(seat)
-                                        break
-                                    case 'bottom_alt':
-                                        me.seats.bottom_alt.data.push(seat)
-                                        break
-                                    case 'bottom_alt_2':
-                                        me.seats.bottom_alt_2.data.push(seat)
-                                        break
-                                }
-                                me.ctr++
-                            })
-                        }
-                    }).catch(err => {
-                        me.$store.state.errorList = err.response.data.errors
-                        me.$store.state.errorStatus = true
-                    }).then(() => {
-                        setTimeout( () => {
-                            me.loader(false)
-                            me.hasFetchSeat = true
-                        }, 500)
-                    })
-                } else {
-                    me.hasFetchSeat = false
-                }
-            }
+  import SeatPlanBox from './SeatPlanBox'
+  export default {
+    components: {
+      SeatPlanBox
+    },
+    props: {
+      customer: {
+        default: null
+      },
+      onlineClass: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data() {
+      return {
+        assignType: 0,
+        ctr: 0,
+        temp: [],
+        hasCustomer: false,
+        hasCancel: false,
+        message: '',
+        hasFetchSeat: false,
+        seats: {
+          left: {
+            position: 'left',
+            data: []
+          },
+          right: {
+            position: 'right',
+            data: []
+          },
+          bottom: {
+            position: 'bottom',
+            data: []
+          },
+          bottom_alt: {
+            position: 'bottom_alt',
+            data: []
+          },
+          bottom_alt_2: {
+            position: 'bottom_alt_2',
+            data: []
+          }
         }
+      }
+    },
+    methods: {
+      toggleOnlineAttendance() {
+        const me = this
+        me.$store.state.onlineAttendanceLayoutStatus = true
+      },
+      fetchSeats(schedule_id, id) {
+        const me = this
+        let layout = `layout_${id}`,
+          temp_ctr = 0
+        me.seats = {
+          left: { position: 'left', layout: layout, data: [] },
+          right: { position: 'right', layout: layout, data: [] },
+          bottom: { position: 'bottom', layout: layout, data: [] },
+          bottom_alt: { position: 'bottom_alt', layout: layout, data: [] },
+          bottom_alt_2: { position: 'bottom_alt_2', layout: layout, data: [] }
+        }
+        if (schedule_id != null) {
+          me.loader(true)
+          me.$axios
+            .get(`api/seats?studio_id=${id}&scheduled_date_id=${schedule_id}`)
+            .then(res => {
+              if (res.data) {
+                me.temp = res.data.seats
+                me.temp.forEach((seat, index) => {
+                  if (seat.bookings.length > 0) {
+                    temp_ctr += 1
+                  }
+                  switch (seat.position) {
+                    case 'left':
+                      me.seats.left.data.push(seat)
+                      break
+                    case 'right':
+                      me.seats.right.data.push(seat)
+                      break
+                    case 'bottom':
+                      me.seats.bottom.data.push(seat)
+                      break
+                    case 'bottom_alt':
+                      me.seats.bottom_alt.data.push(seat)
+                      break
+                    case 'bottom_alt_2':
+                      me.seats.bottom_alt_2.data.push(seat)
+                      break
+                  }
+                  me.ctr++
+                })
+              }
+            })
+            .catch(err => {
+              me.$store.state.errorList = err.response.data.errors
+              me.$store.state.errorStatus = true
+            })
+            .then(() => {
+              setTimeout(() => {
+                me.loader(false)
+                me.hasFetchSeat = true
+              }, 500)
+            })
+        } else {
+          me.hasFetchSeat = false
+        }
+      }
     }
+  }
 </script>
--- components/modals/BookerCancelClass.vue
+++ components/modals/BookerCancelClass.vue
@@ -1,80 +1,103 @@
 <template>
-    <div class="default_modal">
-        <div class="background" @click.once="toggleClose(false)"></div>
-        <div class="confirmation_wrapper">
-            <div class="confirmation_text">
-                Are you sure you want to cancel this class?
-            </div>
-            <div class="button_group">
-                <a href="javascript:void(0)" class="action_cancel_btn" @click.once="toggleClose(false)">Cancel</a>
-                <div class="action_success_btn confirm margin alternate" @click.once="toggleClose(true)">Confirm</div>
-            </div>
+  <div class="default_modal">
+    <div class="background" @click.once="toggleClose(false)"></div>
+    <div class="confirmation_wrapper">
+      <div class="confirmation_text">
+        Are you sure you want to cancel this class?
+      </div>
+      <div class="button_group">
+        <a
+          href="javascript:void(0)"
+          class="action_cancel_btn"
+          @click.once="toggleClose(false)"
+        >
+          Cancel
+        </a>
+        <div
+          class="action_success_btn confirm margin alternate"
+          @click.once="toggleClose(true)"
+        >
+          Confirm
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            scheduled_date_id: {
-                type: String,
-                default: null
-            }
-        },
-        methods: {
-            toggleClose (status) {
-                const me = this
-                if (status) {
-                    let token = me.$cookies.get('70hokcotc3hhhn5')
-                    me.loader(true)
-                    me.$axios.delete(`api/schedules/${me.scheduled_date_id}`, {
-                        headers: {
-                            Authorization: `Bearer ${token}`
-                        }
-                    }).then(res => {
-                        if (res.data) {
-                            setTimeout( () => {
-                                me.$parent.cancel = false
-                                me.$parent.actionMessage = 'Schedule has been successfully cancelled'
-                                me.$store.state.promptBookerActionStatus = true
-                            }, 500)
-                        }
-                    }).catch(err => {
-                        me.$store.state.errorOverlayStatus = true
-                        me.$store.state.errorList = err.response.data.errors
-                        me.$store.state.errorStatus = true
-                    }).then(() => {
-                        setTimeout( () => {
-                            document.querySelectorAll('.content_wrapper .class_accordion').forEach((value, index) => {
-                                value.classList.remove('toggled')
-                                value.querySelector('.accordion_content').style.height = 0
-                            })
-                            if (me.$parent.studio.online_class) {
-                                if (document.getElementById('online')) {
-                                    document.getElementById('online').click()
-                                }
-                            } else {
-                                setTimeout(() => {
-                                    if (me.$parent.$refs.plan) {
-                                        me.$parent.$refs.plan.fetchSeats(null, me.$parent.studioID)
-                                        document.querySelector('.plan_wrapper').style.transform = `matrix(0.4, 0, 0, 0.4, ${me.$parent.customWidth}, ${me.$parent.customHeight})`
-                                    }
-                                }, 10)
-                            }
-                            me.loader(false)
-                        }, 500)
-                    })
-
+  export default {
+    props: {
+      scheduled_date_id: {
+        type: String,
+        default: null
+      }
+    },
+    methods: {
+      toggleClose(status) {
+        const me = this
+        if (status) {
+          let token = me.$cookies.get('70hokcotc3hhhn5')
+          me.loader(true)
+          me.$axios
+            .delete(`api/schedules/${me.scheduled_date_id}`, {
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            })
+            .then(res => {
+              if (res.data) {
+                setTimeout(() => {
+                  me.$parent.cancel = false
+                  me.$parent.actionMessage =
+                    'Schedule has been successfully cancelled'
+                  me.$store.state.promptBookerActionStatus = true
+                }, 500)
+              }
+            })
+            .catch(err => {
+              me.$store.state.errorOverlayStatus = true
+              me.$store.state.errorList = err.response.data.errors
+              me.$store.state.errorStatus = true
+            })
+            .then(() => {
+              setTimeout(() => {
+                document
+                  .querySelectorAll('.content_wrapper .class_accordion')
+                  .forEach((value, index) => {
+                    value.classList.remove('toggled')
+                    value.querySelector('.accordion_content').style.height = 0
+                  })
+                if (me.$parent.studio.online_class) {
+                  if (document.getElementById('online')) {
+                    document.getElementById('online').click()
+                  }
                 } else {
-                    me.$parent.scheduledDateID = 0
-                    me.$store.state.scheduleID = 0
-                    me.$parent.cancel = false
-                    document.body.classList.remove('no_scroll')
+                  setTimeout(() => {
+                    if (me.$parent.$refs.plan) {
+                      me.$parent.fetchWaitlist(me.$store.state.scheduleID)
+                      me.$parent.$refs.plan.fetchSeats(
+                        null,
+                        me.$parent.studioID
+                      )
+                      document.querySelector(
+                        '.plan_wrapper'
+                      ).style.transform = `matrix(0.4, 0, 0, 0.4, ${me.$parent.customWidth}, ${me.$parent.customHeight})`
+                    }
+                  }, 10)
                 }
-            }
-        },
-        mounted () {
-            document.body.classList.add('no_scroll')
+                me.loader(false)
+              }, 500)
+            })
+        } else {
+          me.$parent.scheduledDateID = 0
+          me.$store.state.scheduleID = 0
+          me.$parent.cancel = false
+          document.body.classList.remove('no_scroll')
         }
+      }
+    },
+    mounted() {
+      document.body.classList.add('no_scroll')
     }
+  }
 </script>
